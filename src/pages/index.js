9a5b0e2cf367e8763af0356c6270aeb9f367b89c
James-Soto <james@gosoto.co>
2022-03-29 10:49:08 +0100

    Tidy up and holding

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,23 +8,9 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to Jammy Biscuit</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    <h1>Jammy Biscuit</h1>
+    <h3>Web & Wordpress<h3>
+    <p>Coming Soon.</p>
   </Layout>
 )
 
